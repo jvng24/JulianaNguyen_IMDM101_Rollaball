ddc9d02a371bc18380a30c8e2b75135b1c0aed4e
jvng24 <jnguye31@terpmail.umd.edu>
2024-02-29 14:17:39 -0500

    Initial commit

---
+++ RollaBall/Assets/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+ // Update is called once per frame
+ void Update()
+    {
+ // Rotate the object on X, Y, and Z axes by specified amounts, adjusted for frame rate.
+        transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+    }
+ 
+}
